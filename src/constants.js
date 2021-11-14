f54c3a8f578e34a4f41d5675c6ed6983704c674f
Kevin Cearns <kcearns@gmail.com>
2021-11-14 09:59:51 -0500

    first commit

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xE8C2B6CEDB3b851caeA002a3732Ca4E47374F615'
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData }
